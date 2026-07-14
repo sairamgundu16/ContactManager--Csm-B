a225087962e017048a21274712c62c750043b3fb
sairamgundu16 <sairam.gundu24@sasi.ac.in>
2026-07-14 12:03:01 +0530

    Update contactApi.js

--- src/api/contactApi.js
+++ src/api/contactApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://contactmanagerbackend-jism.onrender.com"
+    baseURL: "https://contactmanager-csmb-2.onrender.com"
 });
 
 export default API;
